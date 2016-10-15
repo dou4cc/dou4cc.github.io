738c6c4e7574a6a87913858aeadd76b28761afc4
dou4cc <dou4cc@live.cn>
2016-10-15 16:43:37 +0800

    .

--- boot.js
+++ boot.js
@@ -1,1 +1,8 @@
-﻿;
+﻿const song = document.createElement("audio");
+song.src = "/code/take-me-to-your-heart.mp3";
+document.body.append(song);
+song.play();
+return () => {
+	song.pause();
+	document.body.removeChild(song);
+};
