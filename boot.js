03e5bfd7abb100a91a8a26714cd66f7057c30740
dou4cc <dou4cc@live.cn>
2017-03-05 13:11:54 +0800

    Update boot.js

--- boot.js
+++ boot.js
@@ -35,238 +35,8 @@ const multi_key_map = () => {
 };
 library.multi_key_map = multi_key_map;
 
-const tube = (() => {
-	const dp = new WeakMap;
-	return source => dp.get(source) || (() => {
-		const storage = multi_key_map();
-		const tube0 = (...condition) => {
-			const sync = () => {
-				state = storage.get(...condition);
-				if(state){
-					state.handle_num += 1;
-				}else{
-					const [hell0, resolve] = hell();
-					state = {
-						alive: true,
-						thunk: genfn2tick(function*(...args){
-							if(yield hell0 || args.length > 0) (yield hell0)(...args);
-						}),
-						handle_num: 1,
-						listener_num: 0,
-						cache: new Set,
-					};
-					storage.set(...condition, state);
-					resolve(source(...condition));
-				}
-			};
-			const unsync = () => {
-				state.handle_num -= 1;
-				if(state.handle_num === 0){
-					state.alive = false;
-					storage.set(...condition, undefined);
-					state.thunk();
-				}
-			};
-			const listen = listener => {
-				const f0 = listener => {
-					const update = () => {
-						const f = (a, b) => a.length === 0 || a[0] === b[0] && f(a.slice(1), b.slice(1));
-						if(!solution1 || solution.length !== solution1.length || !f(solution, solution1)){
-							lock = true;
-							genfn2tick(function*(){
-								const [hell0, resolve] = hell();
-								setTimeout(resolve, 0);
-								yield (cancel || (() => {}))();
-								solution1 = solution;	
-								cancel = listener(...solution1);
-								yield hell0;
-								update();
-							})();
-						}else{
-							lock = false;
-						}
-					};
-					let lock = false;
-					let cancel;
-					let solution1;
-					return [
-						(...solution1) => {
-							solution = solution1;
-							if(!lock) update();
-						},
-						() => {
-							const [hell0, resolve] = hell();
-							listener = () => resolve();
-							if(solution){
-								solution1 = null;
-								if(!lock) update();
-							}
-							return hell0;
-						},
-					];
-				};
-				const f1 = () => {
-					let used = false;
-					return genfn2tick(function*(){
-						if(!used){
-							used = true;
-							const hell0 = cancel();
-							const listen = listener1 => {
-								clearTimeout(timer);
-								state.cache.delete(listen);
-								[listener, cancel] = f0(listener1);
-								if(solution) listener(...solution);
-								return f1();
-							};
-							state.cache.add(listen);
-							const timer = setTimeout(genfn2tick(function*(){
-								state.cache.delete(listen);
-								state.listener_num -= 1;
-								yield (yield thunk || (() => {}))();
-								listener_num -= 1;
-								if(listener_num === 0) unsync();
-							}), 0);
-							yield hell0;
-						}
-					});
-				};
-				used = true;
-				listener_num += 1;
-				state.listener_num += 1;
-				let solution;
-				let cancel;
-				[listener, cancel] = f0(listener);
-				const thunk = state.thunk((...solution) => listener(...solution));
-				return f1();
-			};
-			let state;
-			let used = false;
-			let listener_num = 0;
-			sync();
-			return listener => {
-				if(listener){
-					if(!state || !state.alive) sync();
-					if(state.listener_num > 0){
-						for(let thunk of state.cache) return thunk(listener);
-						const [hell0, resolve] = hell();
-						let timer = setTimeout(() => {
-							timer = null;
-							for(let thunk of state.cache) return resolve(thunk(listener));
-							resolve(listen(listener));
-						}, 0);
-						return () => {
-							if(timer){
-								clearTimeout(timer);
-							}else{
-								hell0(thunk => thunk());
-							}
-						};
-					}
-					return listen(listener);
-				}
-				setTimeout(() => {
-					if(!used){
-						used = true;
-						unsync();
-					}
-				}, 0);
-			};
-		};
-		dp.set(tube0, tube0);
-		dp.set(source, tube0);
-		return tube0;
-	})();
-})();
-library.tube = tube;
-
-const tubeline = (() => {
-	const dp = new WeakMap;
-	return (...tubes) => {
-		const tubeline0 = (() => {
-			switch(tubes.length){
-			case 0:
-				return tube((...args) => listener => {
-					if(listener) listener(...args);
-				});
-			case 1:
-				return dp.get(tubes[0]) || (() => {
-					const solutions = [];
-					const comparer = ([a], [b]) => a - b;
-					const tube0 = tube((...condition) => {
-						const thunk = tube(tubes[0])(...condition);
-						return listener => {
-							if(listener){
-								let used = false;
-								const thunk1 = thunk((...solution) => {
-									used = true;
-									listener(performance.now(), ...solution);
-								});
-								if(!used && solutions.length > 0) listener(...solutions.pop());
-								return thunk1;
-							}
-							thunk();
-						};
-					});
-					const tube1 = tube((...condition) => {
-						const thunk = tube0(...condition);
-						return listener => {
-							if(listener) return thunk((stamp, ...solution) => {
-								listener(...solution);
-								return () => {
-									solutions.push([stamp, ...solution]);
-									solutions.sort(comparer);
-								};
-							});
-							thunk();
-						};
-					});
-					return tube1;
-				})();
-			case 2:
-				tubes = tubes.map(tube0 => tubeline(tube0));
-				return tube((...condition) => {
-					const listen = listener => {
-						cancel_map.set(listener, () => {
-							cancel_map.delete(listener);
-							run(() => thunk);
-						});
-						const thunk = thunk1(listener);
-					};
-					const listener_set = new Set;
-					const cancel_map = new Map;
-					const thunk0 = tubes[0](...condition)((...args) => {
-						thunk1 = tubes[1](...args);
-						[...listener_set].forEach(listener => listen(listener));
-						return () => {
-							thunk1 = null;
-							cancel_map.forEach(cancel => cancel());
-						};
-					});
-					let thunk1;
-					return listener => {
-						if(listener){
-							listener_set.add(listener);
-							if(thunk1) listen(listener);
-							return () => {
-								listener_set.delete(listener);
-								cancel_map.get(listener)();
-							};
-						}
-						thunk0();
-					};
-				});
-			default:
-				return tubeline(tubes[0], tubeline(...tubes.slice(1)));
-			}
-		})();
-		dp.set(tubeline0, tubeline0);
-		return tubeline0;
-	};
-})();
-library.tubeline = tubeline;
-
 const db = (() => {
-	const hub_source = `({run, hell, is_hell, tickline, gen2tick, genfn2tick, prom2hell}) => port => ({
+	const hub_source = `({tickline, genfn2tick}) => port => ({
 		send: (...list) => {
 			tickline(port => port.postMessage(list))(port);
 		},
@@ -591,6 +361,236 @@ const db = (() => {
 	};
 })();
 library.db = db;
+
+const tube = (() => {
+	const dp = new WeakMap;
+	return source => dp.get(source) || (() => {
+		const storage = multi_key_map();
+		const tube0 = (...condition) => {
+			const sync = () => {
+				state = storage.get(...condition);
+				if(state){
+					state.handle_num += 1;
+				}else{
+					const [hell0, resolve] = hell();
+					state = {
+						alive: true,
+						thunk: genfn2tick(function*(...args){
+							if(yield hell0 || args.length > 0) (yield hell0)(...args);
+						}),
+						handle_num: 1,
+						listener_num: 0,
+						cache: new Set,
+					};
+					storage.set(...condition, state);
+					resolve(source(...condition));
+				}
+			};
+			const unsync = () => {
+				state.handle_num -= 1;
+				if(state.handle_num === 0){
+					state.alive = false;
+					storage.set(...condition, undefined);
+					state.thunk();
+				}
+			};
+			const listen = listener => {
+				const f0 = listener => {
+					const update = () => {
+						const f = (a, b) => a.length === 0 || a[0] === b[0] && f(a.slice(1), b.slice(1));
+						if(!solution1 || solution.length !== solution1.length || !f(solution, solution1)){
+							lock = true;
+							genfn2tick(function*(){
+								const [hell0, resolve] = hell();
+								setTimeout(resolve, 0);
+								yield (cancel || (() => {}))();
+								solution1 = solution;	
+								cancel = listener(...solution1);
+								yield hell0;
+								update();
+							})();
+						}else{
+							lock = false;
+						}
+					};
+					let lock = false;
+					let cancel;
+					let solution1;
+					return [
+						(...solution1) => {
+							solution = solution1;
+							if(!lock) update();
+						},
+						() => {
+							const [hell0, resolve] = hell();
+							listener = () => resolve();
+							if(solution){
+								solution1 = null;
+								if(!lock) update();
+							}
+							return hell0;
+						},
+					];
+				};
+				const f1 = () => {
+					let used = false;
+					return genfn2tick(function*(){
+						if(!used){
+							used = true;
+							const hell0 = cancel();
+							const listen = listener1 => {
+								clearTimeout(timer);
+								state.cache.delete(listen);
+								[listener, cancel] = f0(listener1);
+								if(solution) listener(...solution);
+								return f1();
+							};
+							state.cache.add(listen);
+							const timer = setTimeout(genfn2tick(function*(){
+								state.cache.delete(listen);
+								state.listener_num -= 1;
+								yield (yield thunk || (() => {}))();
+								listener_num -= 1;
+								if(listener_num === 0) unsync();
+							}), 0);
+							yield hell0;
+						}
+					});
+				};
+				used = true;
+				listener_num += 1;
+				state.listener_num += 1;
+				let solution;
+				let cancel;
+				[listener, cancel] = f0(listener);
+				const thunk = state.thunk((...solution) => listener(...solution));
+				return f1();
+			};
+			let state;
+			let used = false;
+			let listener_num = 0;
+			sync();
+			return listener => {
+				if(listener){
+					if(!state || !state.alive) sync();
+					if(state.listener_num > 0){
+						for(let thunk of state.cache) return thunk(listener);
+						const [hell0, resolve] = hell();
+						let timer = setTimeout(() => {
+							timer = null;
+							for(let thunk of state.cache) return resolve(thunk(listener));
+							resolve(listen(listener));
+						}, 0);
+						return () => {
+							if(timer){
+								clearTimeout(timer);
+							}else{
+								hell0(thunk => thunk());
+							}
+						};
+					}
+					return listen(listener);
+				}
+				setTimeout(() => {
+					if(!used){
+						used = true;
+						unsync();
+					}
+				}, 0);
+			};
+		};
+		dp.set(tube0, tube0);
+		dp.set(source, tube0);
+		return tube0;
+	})();
+})();
+library.tube = tube;
+
+const tubeline = (() => {
+	const dp = new WeakMap;
+	return (...tubes) => {
+		const tubeline0 = (() => {
+			switch(tubes.length){
+			case 0:
+				return tube((...args) => listener => {
+					if(listener) listener(...args);
+				});
+			case 1:
+				return dp.get(tubes[0]) || (() => {
+					const solutions = [];
+					const comparer = ([a], [b]) => a - b;
+					const tube0 = tube((...condition) => {
+						const thunk = tube(tubes[0])(...condition);
+						return listener => {
+							if(listener){
+								let used = false;
+								const thunk1 = thunk((...solution) => {
+									used = true;
+									listener(performance.now(), ...solution);
+								});
+								if(!used && solutions.length > 0) listener(...solutions.pop());
+								return thunk1;
+							}
+							thunk();
+						};
+					});
+					const tube1 = tube((...condition) => {
+						const thunk = tube0(...condition);
+						return listener => {
+							if(listener) return thunk((stamp, ...solution) => {
+								listener(...solution);
+								return () => {
+									solutions.push([stamp, ...solution]);
+									solutions.sort(comparer);
+								};
+							});
+							thunk();
+						};
+					});
+					return tube1;
+				})();
+			case 2:
+				tubes = tubes.map(tube0 => tubeline(tube0));
+				return tube((...condition) => {
+					const listen = listener => {
+						cancel_map.set(listener, () => {
+							cancel_map.delete(listener);
+							run(() => thunk);
+						});
+						const thunk = thunk1(listener);
+					};
+					const listener_set = new Set;
+					const cancel_map = new Map;
+					const thunk0 = tubes[0](...condition)((...args) => {
+						thunk1 = tubes[1](...args);
+						[...listener_set].forEach(listener => listen(listener));
+						return () => {
+							thunk1 = null;
+							cancel_map.forEach(cancel => cancel());
+						};
+					});
+					let thunk1;
+					return listener => {
+						if(listener){
+							listener_set.add(listener);
+							if(thunk1) listen(listener);
+							return () => {
+								listener_set.delete(listener);
+								cancel_map.get(listener)();
+							};
+						}
+						thunk0();
+					};
+				});
+			default:
+				return tubeline(tubes[0], tubeline(...tubes.slice(1)));
+			}
+		})();
+		dp.set(tubeline0, tubeline0);
+		return tubeline0;
+	};
+})();
+library.tubeline = tubeline;
 
 const fetch_tube = (() => {
 })();
